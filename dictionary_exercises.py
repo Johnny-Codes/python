f7d523517aa078bc5212d031c5e645ff5d878a66
paul <johnspaulm@gmail.com>
2023-11-21 18:23:35 -0600

    python exercises

---
+++ dictionary_exercises.py
@@ -0,0 +1,30 @@
+# create an empty dictionary
+# create a dictionary with some key-value pairs
+# access each key and value in the dictionary
+# access each key and value in the dictionary using a for loop
+# add a new key-value pair to the dictionary
+# get a value from the dictionary using the key
+# remove a key-value pair from the dictionary
+# update a key-value pair in the dictionary
+# create another dictionary
+# merge the two dictionaries
+# print the merged dictionary
+# print the keys of the merged dictionary
+# print the values of the merged dictionary
+# print the items of the merged dictionary
+# create a list
+# add a new key to the dictionary where the value is the list
+# print the dictionary
+# print the list in the dictionary
+# print the values in the list in the dictionary
+# create a list of keys ie ["name", "age", "state"]
+# using fromkeys(), create a dictionary where the keys are the list of keys
+# delete the dictionary
+# create an empty dictionary
+# set defaults for the keys in the list you created
+# create a dictionary where the keys are the list of keys
+# create a dictionary with nested dictionaries and lists
+# print the dictionary
+# print the list in the dictionary
+# print the values in the list in the dictionary
+# look up more methods for dictionaries and do more exercises
