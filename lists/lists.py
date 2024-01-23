223dabc7953a22463866b351d4ed2e356494fa9c
Paul Johns <johnspaulm@gmail.com>
2024-01-23 04:59:48 -0600

    initial lists commit

---
+++ lists/lists.py
@@ -0,0 +1,25 @@
+# Python Lists
+
+# References:i
+# https://docs.python.org/3/tutorial/datastructures.html
+# https://python-reference.readthedocs.io/en/latest/docs/list/index.html
+
+# Common List Methods
+#
+#
+#
+
+
+# Create an empty list
+
+groceries = []
+
+# add items to list
+
+groceries.append("bananas")
+
+# Remove items from list
+
+
+
+# List slicing
